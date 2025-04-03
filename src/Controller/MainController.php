a10bdb2f413b9f19082ab2453359d055cb1bbe42
Jakub Idzikowski <jakub.idzikowski@gmail.com>
2025-04-03 23:57:41 +0200

    added controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class MainController {
 
+  #[Route('/')]
+  public function homepage() {
+    return new Response('<strong>Hello</hello> world!');
+  }
 }
