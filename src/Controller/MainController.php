5749a0f0731608e59c1c6e924c5a1895f2423620
Jakub Idzikowski <jakub.idzikowski@gmail.com>
2025-04-04 13:49:37 +0200

    added twig layout/parent template

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,12 +4,28 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
-    public function homepage()
+    public function homepage(): Response
     {
-        return new Response('<strong>Hello</hello> world!');
+
+        $starshipCount = 400;
+
+        // tablica asocjacyjna
+        $myShip = [
+            'name' => 'USS LeafyCruiser',
+            'class' => 'Garden',
+            'captain' => 'Michael Jackson',
+            'status' => 'still building...',
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip,
+        ]);
+        // return new Response('<strong>Hello</hello> world!');
     }
 }
